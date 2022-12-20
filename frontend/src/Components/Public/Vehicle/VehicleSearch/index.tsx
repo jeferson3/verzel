db7f653262807e48602d434ef490995ad0020036
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2022-12-19 21:56:35 -0300

    concluindo área públuca

--- frontend/src/Components/Public/Vehicle/VehicleSearch/index.tsx
+++ frontend/src/Components/Public/Vehicle/VehicleSearch/index.tsx
@@ -1,31 +1,37 @@
 import "./style.css";
-import {Form, InputGroup} from "react-bootstrap";
+import {Button, Form, InputGroup} from "react-bootstrap";
 import {useVehicleContext} from "../../../../Context/Public/VehicleContext/context";
 import {getVehicles} from "../../../../Context/Public/VehicleContext/actions";
 import React, {useState} from "react";
 
 export const VehicleSearch: React.FC = () => {
 
-
-    const {state: {vehicles: {page, pages}}, dispatch} = useVehicleContext();
-    const totalPages = Object.keys(new Array(pages).fill(null)).map(Number)
+    const {dispatch} = useVehicleContext();
+    const [search, setSearch] = useState("");
 
     const searchVehicles = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const search = e.target.value;
+        setSearch(e.target.value);
 
-        if (search.length > 3) {
+        if (e.target.value.length > 3) {
             getVehicles(1, 5, {search}, dispatch);
         }
+    }
+
+    const clearSearch = () => {
+        setSearch("");
+        getVehicles(1, 5, {}, dispatch);
     }
 
     return (
 
         <div>
             <InputGroup className="mb-3">
+                {search !== "" && <Button variant='secondary' onClick={clearSearch}>Limpar</Button>}
                 <Form.Control
                     aria-label="Small"
                     aria-describedby="inputGroup-sizing-sm"
                     onChange={searchVehicles}
+                    value={search}
                 />
                 <InputGroup.Text id="inputGroup-sizing-sm">
                     <i className="fas fa-search"></i>
